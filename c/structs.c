f0ac9fe4a5b358c4b1c785bf20deec400d98882d
Cristian <cvg97@misena.edu.co>
2020-08-06 08:07:44 +0000

    more structs.c

--- c/structs.c
+++ c/structs.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+void structObjectFunc(void);
+void structPointerFunc(void);
+
 int main(void){
-    struct object {
-	int x;
-	int y;
+    structObjectFunc();
+    structPointerFunc();
+
+}
+
+
+void structObjectFunc(){
+   struct object {
+        int x;
+        int y;
     };
 
     struct object myObject;
@@ -12,3 +22,21 @@ int main(void){
    printf(".x = %i, .y = %i\n", myObject.x, myObject.y);
 
 }
+
+
+void structPointerFunc(){
+    struct object {
+       int x;
+       int y;
+    } myObject;
+
+   struct object *p = &myObject;
+
+   p->x = 42;
+   p->y = 123;
+
+   printf(".x = %i, y = %i\n", p->x, p->y);
+   printf(".X = %i, y = %i\n", myObject.x, myObject.y);
+}
+
+
