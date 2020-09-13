8fc53abd2a3918b92b9cbb2e6801eaad376c269f
Cristian Vargas <cvg97@misena.edu.co>
2020-09-13 17:12:23 +0000

    linkedlist

--- c/linkedlist/linkedlist.h
+++ c/linkedlist/linkedlist.h
@@ -1,25 +1,39 @@
-#include <sdtlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 
 struct list {
-	void *data; // generic data 
+	void *data; // generic data
 	struct list *next;
 };
 
 
-void push(struct list ** head, void *data, size_t data_size){
-	struct list * node = malloc(sizeof(struct list));
+void push(struct list **head, void *data, size_t data_size){
+	struct list *node = (struct list*)malloc(sizeof(struct list));
 	node->data = malloc(data_size);
 	node->next = (*head);
 
-	int i; for(i=0; i < data_size; i++){
+	int i;
+ 	for(i=0; i < data_size; i++){
 		*(char *)(node->data + i) = *(char *)(data + 1);
 		(*head) = node;
 	}
 }
 
 
-void printFloat(void *){
+void printList(struct list *node, void (*fptr)(void *)){
+	while(node != NULL){
+		(*fptr)(node->data);
+		node = node->next;
+	}
+}
+
+
+void printFloat(void *f){
 	printf("%f", *(float *)f);
+}
+
+
+void printInt(void *n){
+	printf(" %d", *(int *)n);
 }
